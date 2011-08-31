56aeb00c99b1356cd7d580c976e6d191489344fe
Allen Byrne <byrn@hdfgroup.org>
2011-08-31 10:05:16 -0500

    Add cmake cache configure file for simpler windows auto testing

---
+++ 1_8/config/cmake/cacheinit.cmake
@@ -0,0 +1,11 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+SET (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
+
+SET (BUILD_TESTING ON CACHE BOOL "Build HDF5 Unit Testing" FORCE)
+
+SET (HDF5_ENABLE_F2003 OFF CACHE BOOL "Enable FORTRAN 2003 Standard" FORCE)
