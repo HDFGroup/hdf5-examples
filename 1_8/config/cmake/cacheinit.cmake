991c99517e891bc17f64b3b1a9482952ce3905ad
Allen Byrne <byrn@hdfgroup.org>
2015-04-02 14:53:51 -0500

    Upgrade to latest internal standards

--- 1_8/config/cmake/cacheinit.cmake
+++ 1_8/config/cmake/cacheinit.cmake
@@ -4,7 +4,7 @@
 # EXTERNAL cache entries
 ########################
 
-set (USE_SHARED_LIBS ON CACHE BOOL "Use Shared Libraries" FORCE)
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
 
 set (HDF_BUILD_FORTRAN OFF CACHE BOOL "Build FORTRAN support" FORCE)
 
@@ -12,7 +12,7 @@ set (BUILD_TESTING ON CACHE BOOL "Build HDF5 Unit Testing" FORCE)
 
 set (HDF_ENABLE_PARALLEL OFF CACHE BOOL "Enable parallel build (requires MPI)" FORCE)
 
-set (HDF_ENABLE_F2003 OFF CACHE BOOL "Enable FORTRAN 2003 Standard" FORCE)
+set (HDF_ENABLE_F2003 ON CACHE BOOL "Enable FORTRAN 2003 Standard" FORCE)
 
 set (HDF_ENABLE_THREADSAFE OFF CACHE BOOL "(WINDOWS)Enable Threadsafety" FORCE)
 
