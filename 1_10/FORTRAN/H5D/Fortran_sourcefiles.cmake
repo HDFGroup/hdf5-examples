02965c90576684c80c7a29a484f47cbbde52dca6
Allen Byrne <byrn@hdfgroup.org>
2017-03-13 15:38:09 -0500

    HDFFV-10138 - update to new format app framework

---
+++ 1_10/FORTRAN/H5D/Fortran_sourcefiles.cmake
@@ -0,0 +1,23 @@
+#-----------------------------------------------------------------------------
+# Define Sources, one file per application
+#-----------------------------------------------------------------------------
+set (examples
+    h5ex_d_alloc.f90
+    h5ex_d_checksum.f90
+    h5ex_d_chunk.f90
+    h5ex_d_compact.f90
+    h5ex_d_extern.f90
+    h5ex_d_fillval.f90
+    h5ex_d_gzip.f90
+    h5ex_d_hyper.f90
+#    h5ex_d_nbit.f90
+    h5ex_d_rdwr.f90
+#    h5ex_d_soint.f90
+#    h5ex_d_szip.f90
+    h5ex_d_transform.f90
+    h5ex_d_unlimmod.f90
+#    h5ex_d_shuffle.f90
+#    h5ex_d_sofloat.f90
+#    h5ex_d_unlimadd.f90
+#    h5ex_d_unlimgzip.f90
+)
