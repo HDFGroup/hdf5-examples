786b7b3bfefc329c1270a6df3b3589e3ef1615db
Allen Byrne <byrn@hdfgroup.org>
2018-02-27 17:19:16 -0600

    Add the Threads libs

--- 1_8/config/cmake/cacheinit.cmake
+++ 1_8/config/cmake/cacheinit.cmake
@@ -14,7 +14,7 @@ set (HDF_ENABLE_PARALLEL OFF CACHE BOOL "Enable parallel build (requires MPI)" F
 
 set (HDF_ENABLE_F2003 ON CACHE BOOL "Enable FORTRAN 2003 Standard" FORCE)
 
-set (HDF_ENABLE_THREADSAFE OFF CACHE BOOL "(WINDOWS)Enable Threadsafety" FORCE)
+set (HDF_ENABLE_THREADSAFE OFF CACHE BOOL "Enable Threadsafety" FORCE)
 
 set (HDF5_PACKAGE_NAME "hdf5" CACHE STRING "Name of HDF5 package" FORCE)
 
