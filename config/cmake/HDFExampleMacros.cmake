0ca570a2d5e3750301b395ef54b0b1355734b8b6
Allen Byrne <byrn@hdfgroup.org>
2023-09-06 08:51:11 -0500

    Add 116 API option and files

--- config/cmake/HDFExampleMacros.cmake
+++ config/cmake/HDFExampleMacros.cmake
@@ -416,4 +416,12 @@ macro (APIVersion version xyapi)
   if (${EXAMPLE_VARNAME}_USE_114_API AND ${xyapi} GREATER 114)
     set (${xyapi} "114")
   endif ()
+
+  #-----------------------------------------------------------------------------
+  # Option to use 1.16.x API
+  #-----------------------------------------------------------------------------
+  option (${EXAMPLE_VARNAME}_USE_116_API "Use the HDF5 1.16.x API" ON)
+  if (${EXAMPLE_VARNAME}_USE_116_API AND ${xyapi} GREATER 116)
+    set (${xyapi} "116")
+  endif ()
 endmacro ()
