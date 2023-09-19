4fe3604ee930dce3617b7747ae8e0c2a5bb7f47d
Allen Byrne <byrn@hdfgroup.org>
2023-09-19 10:36:16 -0500

    Correct the version check for java reference files

--- JAVA/H5G/Java_sourcefiles.cmake
+++ JAVA/H5G/Java_sourcefiles.cmake
@@ -7,16 +7,18 @@ set (HDF_JAVA_EXAMPLES
     H5Ex_G_Corder.java
     H5Ex_G_Phase.java
 )
-if (${H5_LIBVER_DIR} EQUAL 110)
-  set (HDF_JAVA_EXAMPLES ${HDF_JAVA_EXAMPLES}
-      110/H5Ex_G_Iterate.java
-      110/H5Ex_G_Intermediate.java
-      110/H5Ex_G_Visit.java
-  )
-else ()
-  set (HDF_JAVA_EXAMPLES ${HDF_JAVA_EXAMPLES}
-      H5Ex_G_Iterate.java
-      H5Ex_G_Intermediate.java
-      H5Ex_G_Visit.java
-  )
+if (${H5_LIBVER_DIR} GREATER 18)
+  if (${H5_LIBVER_DIR} EQUAL 110)
+    set (HDF_JAVA_EXAMPLES ${HDF_JAVA_EXAMPLES}
+        110/H5Ex_G_Iterate.java
+        110/H5Ex_G_Intermediate.java
+        110/H5Ex_G_Visit.java
+    )
+  else ()
+    set (HDF_JAVA_EXAMPLES ${HDF_JAVA_EXAMPLES}
+        H5Ex_G_Iterate.java
+        H5Ex_G_Intermediate.java
+        H5Ex_G_Visit.java
+    )
+  endif ()
 endif ()
